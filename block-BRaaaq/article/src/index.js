748184459b7e8acdbd3e1bf37f3765d4b4a89835
seeratbhatia <b.seerat@gmail.com>
2022-07-25 15:30:08 +0530

    components

--- block-BRaaaq/article/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import './styles/index.css';
-// import './components/data';
-
-ReactDOM.render(<App />, document.getElementById('root'));
