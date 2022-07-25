d2b7978e80a163c1203f98ebe33d5ee3aba80023
seeratbhatia <b.seerat@gmail.com>
2022-07-25 14:58:04 +0530

    changes in components

---
+++ block-BRaaaq/article/src/components/Hero.js
@@ -0,0 +1,17 @@
+function Header() {
+    return (
+      <>
+        <div className="hero">
+          <img
+            src="/Images/hero.jpeg"
+            alt=""
+            width="100%"
+            height="350px"
+            background-size="cover"
+          />
+        </div>
+      </>
+    );
+  }
+  
+  export default Header;
