ab9a09ee706ee03c1a89cbbc2315996c0df79d66
seeratbhatia <b.seerat@gmail.com>
2022-07-28 14:07:21 +0530

    block

--- block-BRaaav/assignment1/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import "./stylesheet/html.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
--- block-BRaaav/hyro/src/index.js
+++ block-BRaaav/hyro/src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import './styles/index.css';
-// import './components/data';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
