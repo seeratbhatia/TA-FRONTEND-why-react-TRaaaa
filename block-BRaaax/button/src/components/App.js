533272080d1d336c781345779c8b7625f1a4e152
seeratbhatia <b.seerat@gmail.com>
2022-07-29 00:26:54 +0530

    done

---
+++ block-BRaaax/button/src/components/App.js
@@ -0,0 +1,22 @@
+import "./style.css";
+import Button from "./Button";
+
+function App() {
+  return (
+    <>
+      <Button label="Click Me" />
+      <Button label="Click Me" type="secondary" size="large" />
+      <Button label="Click Me" type="tertiary" size="medium" />
+      <Button />
+      <Button disabled />
+      <Button
+        size="large"
+        label="Button"
+        type="tertiary"
+        onClickHandler={() => alert("You Clicked Me!")}
+      />
+    </>
+  );
+}
+
+export default App;
